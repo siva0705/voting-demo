e9b65dfed1136e0a1e4b603c7f73feaf6ca0060b
root <root@localhost.localdomain>
2018-08-16 07:38:28 +0000

    changing the voting options

--- voting/app.py
+++ voting/app.py
@@ -9,8 +9,8 @@ import logging
 import signal
 import sys
 
-optionA = "Dev"
-optionB = "Ops"
+optionA = "Cricket"
+optionB = "hockey"
 name = optionA + " VS " + optionB
 
 redis = Redis(host="redis", db=0)
